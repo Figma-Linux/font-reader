8605b1ad7607d44a9d225621b1de13477a9f4bab
Chugunov Roman <Zebs-BMK@yandex.ru>
2023-03-22 23:15:28 +0300

    test: fonts

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,35 @@ use font_reader::read;
 
 fn main() {
   let paths = vec![
-    "/usr/share/fonts/volkhov/Volkhov-BoldItalic.ttf",
-    "/usr/share/fonts/users/PTSerifCaption.ttc",
-    "/usr/share/fonts/opentype/malayalam/Manjari-Bold.otf",
+    // "/home/ruut/.local/share/fonts/Segoe/SegoeUI-BoldItalic.ttf",
+    // "/home/ruut/.local/share/fonts/Segoe/SegoeUI-Bold.ttf",
+    // "/home/ruut/.local/share/fonts/Segoe/SegoeUI-Italic.ttf",
+    // "/home/ruut/.local/share/fonts/Segoe/SegoeUI-Light.ttf",
+    // "/home/ruut/.local/share/fonts/Segoe/SegoeUI-SemiBold.ttf",
+    // "/home/ruut/.local/share/fonts/Segoe/SegoeUI.ttf",
+    "/home/ruut/.local/share/fonts/otf/expletus-sans-italic.otf",
     "/home/ruut/.local/share/bad_fonts/Portico Light Rough.otf",
+    "/home/ruut/.local/share/fonts/variable/AngstVF.ttf",
+    "/home/ruut/.local/share/fonts/variable/RobotoFlex[slnt,wdth,wght,opsz].ttf",
+    // "/home/ruut/.local/share/fonts/otf/fundamental-brigade-scvhlank.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoBoldItalic.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoBold.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoHeavyItalic.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoHeavy.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoLightItalic.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoLight.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoMediumItalic.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoMedium.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoRegularItalic.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoRegular.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoSemiboldItalic.otf",
+    // "/home/ruut/.local/share/fonts/otf/SFMonoSemibold.otf",
   ];
 
   for path in paths {
     match read(path) {
       Ok(data) => {
-        println!("Font {}\nFont count: {}", path, data.len(),);
+        println!("Font {} | Font count: {}", path, data.len(),);
 
         for font in data {
           println!(
